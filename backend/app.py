185dca5c04ecce3c6f11801367e81bb903ddc499
aubinoke229-glitch <aubinoke229@gmail.com>
2026-02-21 05:27:22 +0100

    flask

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,16 @@
-           app.run(host="0.0.0.0", port=port)from flask import Flask
-           import os
+import os
+from flask import Flask
 
-           app = Flask(__name__)
+app = Flask(__name__)
 
-           @app.route("/")
-           def home():
-               return "<h1>Africa Academy Certification</h1>"
+@app.route("/")
+def home():
+    return "<h1>Africa Academy Certification</h1>"
 
-               @app.route("/admin")
-               def admin():
-                   return "<h2>Admin Panel</h2>"
+    @app.route("/admin")
+    def admin():
+        return "<h2>Admin Panel</h2>"
 
-                   if __name__ == "__main__":
-                       port = int(os.environ.get("PORT", 10000))
-                           app.run(host="0.0.0.0", port=port)
+        if __name__ == "__main__":
+            port = int(os.environ.get("PORT", 5000))
+                app.run(host="0.0.0.0", port=port)
