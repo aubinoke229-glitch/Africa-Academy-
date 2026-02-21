7abbc917f810e21ab1204d7ae3f92dd7f2b29ee7
aubinoke229-glitch <aubinoke229@gmail.com>
2026-02-21 04:24:52 +0100

    fix app

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -6,9 +7,10 @@ app = Flask(__name__)
 def home():
     return "<h1>Africa Academy Certification Platform</h1><p>Site en ligne ✔</p>"
 
-@app.route("/admin")
-def admin():
-    return "<h2>Admin Panel</h2><p>Connexion fonctionnelle ✔</p>"
+    @app.route("/admin")
+    def admin():
+        return "<h2>Admin Panel</h2><p>Connexion fonctionnelle ✔</p>"
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+        if __name__ == "__main__":
+            port = int(os.environ.get("PORT", 10000))
+                app.run(host="0.0.0.0", port=port)
